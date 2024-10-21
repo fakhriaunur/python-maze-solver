bc99ea729efc098244cdca43a65caddc0aef7f4a
Fakhri <fakhri.aunur@gmail.com>
2024-10-21 21:12:47 +0700

    Revision to the last commit

--- graphics.py
+++ graphics.py
@@ -2,13 +2,13 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window():
     def __init__(self, width: int, height: int):
-        self.width = width
-        self.height = height
         self.__root_widget = Tk()
-        self.__root_widget.title("root")
-        self.canvas = Canvas()
-        self.canvas.pack()
-        self.is_running = False
+        self.__root_widget.title("Maze Solver")
+        self.__canvas = Canvas(
+            self.__root_widget, bg="white", height=height, width=width
+        )
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__is_running = False
         self.__root_widget.protocol("WM_DELETE_WINDOW", self.close)
     
     def redraw(self):
@@ -16,9 +16,10 @@ class Window():
         self.__root_widget.update()
     
     def wait_for_close(self):
-        self.is_running = True
-        while self.is_running:
+        self.__is_running = True
+        while self.__is_running:
             self.redraw()
+        print("window closed...")
     
     def close(self):
-        self.is_running = False
+        self.__is_running = False
