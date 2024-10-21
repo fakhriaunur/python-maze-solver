86d1783b97703272183f8254d33b11af62db8d00
Fakhri <fakhri.aunur@gmail.com>
2024-10-21 21:49:55 +0700

    Add Point and Line

--- graphics.py
+++ graphics.py
@@ -1,5 +1,21 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point():
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, p1: Point, p2: Point):
+        # self.line = Point(p1.x, p2.y)
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
+    
 class Window():
     def __init__(self, width: int, height: int):
         self.__root_widget = Tk()
@@ -23,3 +39,6 @@ class Window():
     
     def close(self):
         self.__is_running = False
+    
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw(self.__canvas, fill_color)
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from graphics import Window
+from graphics import Line, Point, Window
 
 def main():
     win = Window(800, 600)
+    
+    p1 = Point(100, 100)
+    p2 = Point(500, 100)
+    
+    win.draw_line(Line(p1, p2), "red")
+    
     win.wait_for_close()
 
 if __name__ == "__main__":
