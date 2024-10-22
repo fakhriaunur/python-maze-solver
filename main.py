91f68d866cc572fa80ca568a11a0bead5e4398b6
Fakhri <fakhri.aunur@gmail.com>
2024-10-22 15:54:19 +0700

    Revision for the last commit

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
 from cell import Cell
 from graphics import Line, Point, Window
+from maze import Maze
 
 def main():
-    win = Window(800, 600)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
     
-    p1 = Point(100, 100)
-    p2 = Point(500, 100)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+    # win = Window(800, 600)
     
-    line1 = Line(p1, p2)
+    # p1 = Point(100, 100)
+    # p2 = Point(500, 100)
     
-    # win.draw_line(line1, "red")
+    # line1 = Line(p1, p2)
     
-    cell1 = Cell(win)
-    cell1.draw(100, 100, 500, 500)
+    # # win.draw_line(line1, "red")
     
-    cell2 = Cell(win)
-    cell2.draw(200, 200, 300, 300)
+    # cell1 = Cell(win)
+    # cell1.draw(100, 100, 500, 500)
     
-    cell3 = Cell(win)
-    cell3.has_bottom_wall = False
-    cell3.draw(50, 50, 610, 610)
+    # cell2 = Cell(win)
+    # cell2.draw(200, 200, 300, 300)
     
-    cell2.draw_move(cell1)
+    # cell3 = Cell(win)
+    # cell3.has_bottom_wall = False
+    # cell3.draw(50, 50, 610, 610)
     
-    cell1.draw_move(cell3, True)
+    # cell2.draw_move(cell1)
+    
+    # cell1.draw_move(cell3, True)
     
     win.wait_for_close()
 
--- maze.py
+++ maze.py
@@ -11,10 +11,11 @@ class Maze():
         y1: int,
         num_rows: int,
         num_cols: int,
-        cell_size_x: int,
-        cell_size_y: int,
+        cell_size_x,
+        cell_size_y,
         win: Window
     ):
+        self.__cells: List[List[Cell]] = []
         self.x1 = x1
         self.y1 = y1
         self.num_rows = num_rows
@@ -22,24 +23,27 @@ class Maze():
         self.cell_size_x = cell_size_x
         self.cell_size_y = cell_size_y
         self.win = win
+        
         self.__create_cells()
     
     def __create_cells(self):
-        self.__cells: List[List[Cell]] = []
-        for i in range(self.num_rows):
-            rows: List[Cell] = []
-            for j in range(self.num_cols):
+        for i in range(self.num_cols):
+            cols: List[Cell] = []
+            for j in range(self.num_rows):
                 cell = Cell(self.win)
-                rows.append(cell)
-            self.__cells.append(rows)
+                cols.append(cell)
+            self.__cells.append(cols)
         
-        for i in range(self.num_rows):
-            for j in range(self.num_cols):
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
                 self.__draw_cell(i, j)
     
     def __draw_cell(self, i: int, j: int):
-        x1 = self.x1 + (j * self.cell_size_x)
-        y1 = self.y1 + (i * self.cell_size_y)
+        if self.win is None:
+            return
+        
+        x1 = self.x1 + (i * self.cell_size_x)
+        y1 = self.y1 + (j * self.cell_size_y)
         x2 = x1 + self.cell_size_x
         y2 = y1 + self.cell_size_y
         
@@ -48,5 +52,8 @@ class Maze():
         self.__animate()
     
     def __animate(self):
+        if self.win is None:
+            return
+        
         self.win.redraw()
         sleep(0.05)
